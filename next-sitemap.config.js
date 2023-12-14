c0543e484160159584f98131cda0de135d323a59
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2023-12-14 10:16:16 +0100

    🔍 Update sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	siteUrl: "https://quikk.de/",
 	generateRobotsTxt: true,
-	exclude: ["/404", "/api/*", "/datenschutz", "impressum"],
+	exclude: ["/404", "/api/*", "/datenschutz", "impressum", "/team", "/projekte", "/projekte/*", "/karriere", "/blog", "/blog/*"],
 };
